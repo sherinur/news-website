7118a34ca6cd90e000d252e775bfca71b59d69d5
sherinur <sherinurislam@gmail.com>
2024-11-19 17:53:25 +0500

    app

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+const token = "c7f1fc8ca3402d51c5ec4b4fe59672cb"
+
+// async function start() {
+//     try {
+//         const url = "http://api.mediastack.com/v1/news?access_key=" + token + "&languages=en,-de"
+//         const resp = await fetch(url)
+//         const data = await resp.json()
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+
+// start()
